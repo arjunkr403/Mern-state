ea42cd4cac9486bd443c40b138a8e1d44ee3dfe8
arjunkr403 <arjunkr403@gmail.com>
2024-03-30 17:44:36 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+
+export default function App(){
+  return <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<Signin />}/>
+      <Route path="/sign-up" element={<Signup />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+  </Routes>
+  </BrowserRouter>;
 }
