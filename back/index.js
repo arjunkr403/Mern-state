8d58f0a0f9da065ab5622c7d34d6f7eab9a35430
arjunkr403 <arjunkr403@gmail.com>
2024-03-31 22:56:25 +0530

    connect to db

--- back/index.js
+++ back/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to MongoDB");
+}).catch((err)=>{console.log(err);});
 
 const app=express();
 app.listen(3000,()=>{
