7c840519dba241ed0a594325ec17638abefd7e48
arjunkr403 <arjunkr403@gmail.com>
2024-03-31 18:15:13 +0530

    changed directory added express

---
+++ main/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import {FaSearch} from  'react-icons/fa'; 
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+
+    <header className='bg-back shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+         <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+            <span className='text-txt1'>Mern</span>
+            <span className='text-txt2'>Estate</span>
+        </h1>
+         </Link>   
+        <form className='bg-txt2 rounded-lg flex items-center p-3'>
+            <input type="text-rnd" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch className='text-rnd'/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/'>
+            <li className='hidden sm:inline text-txt2 hover:underline'>Home</li>
+            </Link>
+            <Link to='about'>
+            <li className='hidden sm:inline text-txt2 hover:underline'>About</li>
+            </Link>
+            <Link to='sign-in'>
+            <li className='hidden sm:inline text-txt2 hover:underline'>Sign in</li>
+            </Link>
+        </ul>
+        </div>
+    </header>
+  )
+}
---
+++ main/tailwind.config.js
@@ -0,0 +1,33 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//     },
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'back': '#222831',
+        'txt1':'#76ABAE',
+        'txt2':'#EEEEEE',
+        'rnd':'#31363F',
+        // Add more custom colors here if needed
+      },
+    },
+  },
+  plugins: [],
+};
