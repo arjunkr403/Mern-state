e559ac4d6a22e862912c72e6e4c22590a51b662d
arjunkr403 <arjunkr403@gmail.com>
2024-07-05 00:39:13 +0530

    add redux toolkit

--- main/src/pages/Signin.jsx
+++ main/src/pages/Signin.jsx
@@ -1,59 +1,81 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 export default function Signin() {
-    const [formData, setFormData] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const nav = useNavigate();
-    const hchange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value,
-        });
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const nav = useNavigate();
+  const dispatch = useDispatch();
+  const hchange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const hsubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/back/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok || data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      nav("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    const hsubmit = async (e) => {
-        e.preventDefault();
-        try {
-            setLoading(true);
-            const res = await fetch('/back/auth/signin', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            if (data.success === false) {
-                setError(data.message);
-                setLoading(false);
-                return;
-            }
-            setLoading(false);
-            setError(null);
-            nav('/');
-        } catch (error) {
-            setLoading(false);
-            setError(error.message);
-        }
-        // console.log(data);
-    };
-    // console.log(formData);
-    return (
-        <div className="p-3 max-w-lg mx-auto">
-            <h1 className="text-3xl text-rnd text-center font-semibold my-7">Sign In</h1>
-            <form onSubmit={hsubmit} className="flex flex-col gap-4 ">
-                <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={hchange} />
-                <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={hchange} />
-                <button disabled={loading} className="bg-rnd rounded-lg text-txt2 p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...' : 'Sign In'}</button>
-            </form>
-            <div className="flex gap-2 mt-5 ml-1">
-                <p>Don't have an account?</p>
-                <Link to={'/sign-up'}>
-                    <span className="text-blue-700 font-semibold">Sign up</span>
-                </Link>
-            </div>
-            {error && <p className="text-red-500 mt-5">{error}</p>}
-        </div>
-    )
+    // console.log(data);
+  };
+  // console.log(formData);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-rnd text-center font-semibold my-7">
+        Sign In
+      </h1>
+      <form onSubmit={hsubmit} className="flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={hchange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={hchange}
+        />
+        <button
+          disabled={loading}
+          className="bg-rnd my-2 rounded-lg text-txt2 p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 ml-1">
+        <p>Don't have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700 font-semibold">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
--- main/src/pages/Signup.jsx
+++ main/src/pages/Signup.jsx
@@ -24,6 +24,13 @@ export default function Signup() {
                 body: JSON.stringify(formData),
             });
             const data = await res.json();
+            console.log(data);
+            if (!res.ok) { // Check for HTTP errors
+                setError(data.message || "An error occurred");
+                setLoading(false);
+                return;
+              }
+        
             if(data.success===false){
                 setError(data.message);
                 setLoading(false);
@@ -36,9 +43,9 @@ export default function Signup() {
             setLoading(false);
             setError(error.message);
         }
-        console.log(data);
     };
-    console.log(formData);
+    // console.log(formData);
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl text-rnd text-center font-semibold my-7">Sign Up</h1>
@@ -46,7 +53,7 @@ export default function Signup() {
                 <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" onChange={hchange}/>
                 <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={hchange}/>
                 <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={hchange}/>
-                <button disabled={loading} className="bg-rnd rounded-lg text-txt2 p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ?'Loading...': 'Sign Up'}</button>
+                <button disabled={loading} className="bg-rnd my-2 rounded-lg text-txt2 p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ?'Loading...': 'Sign Up'}</button>
             </form>
             <div className="flex gap-2 mt-5 ml-1">
                 <p>Have an account?</p>
