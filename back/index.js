500483c2174111a05537222f7cd5607c06c18b78
arjunkr403 <arjunkr403@gmail.com>
2024-07-17 19:42:09 +0530

    create listing route

--- back/index.js
+++ back/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.listen(3000, () => {
 });
 app.use("/back/user", userRouter);
 app.use("/back/auth", authRouter);
+app.use("/back/listing", listingRouter);
 
 //Middleware
 app.use((err, req, res, next) => {
