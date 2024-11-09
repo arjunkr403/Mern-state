d9e7db70d66577014da4143ecb5515edb635ecd2
arjunkr403 <arjunkr403@gmail.com>
2024-11-09 15:50:03 +0530

    port changes

--- back/index.js
+++ back/index.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
